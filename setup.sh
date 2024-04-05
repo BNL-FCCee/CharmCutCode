26eefb9341061179a412c267fc06a22a485dc866
Haider Abidi <syed.haider.abidi@cern.ch>
2024-04-05 13:22:12 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- setup.sh
+++ setup.sh
@@ -4,5 +4,3 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-centos7-gcc12-opt/setup.sh
 
 
 # this is an auto-generated setup script
-export PATH=${PATH}:/usatlas/u/ivelisce/FCC/CharmCut/source/build/
-export wscanner_data=/usatlas/u/ivelisce/FCC/CharmCut/source/wscanner/data/
